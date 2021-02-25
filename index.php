93988e4dc327d483862e57006d67bc971dc57c78
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-25 21:53:57 +0800

    Update and rename PANDEMIC.HTML to index.php

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
 <body>
 <center>
 <body bgcolor= "lightpink">
-<font color= "black">  Coronavirus disease (COVID-19) <b> is an infectious disease caused by a newly discovered coronavirus. <b>
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/i0ZabxXmH4Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<p> <font color= "black">  Coronavirus disease (COVID-19) <b> is an infectious disease caused by a newly discovered coronavirus. <b>
 Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment.<b> The virus that causes COVID-19 is mainly transmitted through droplets generated when an infected person coughs, sneezes, or exhales.<b> These droplets are too heavy to hang in the air, and quickly fall on floors or surfaces.<b>
 You can be infected by breathing in the virus if you are within close proximity of someone who has COVID-19, or by touching a contaminated surface and then your eyes, nose or mouth.<b> 
 </font> </center>
 <p>
 <p>
-<p align= center>
-<img src= "handz.jpg"> <img src= "VIRUS1.jpg">
+<p align="center"> <img src = "https://www.munsonhealthcare.org/media/Image/Kid%20Resources%20About%20Coronavirus%20356x240(1).jpg" , width = 210 , length = 210></img><br>
+  </center>
+  </body>
+  </html>
