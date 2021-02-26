9980b13819c0ad4c4081f8e45727a78391a5b178
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-26 09:39:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
   <iframe width="560" height="315" src="https://www.youtube.com/embed/i0ZabxXmH4Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <p> <font color= "black">  Coronavirus disease (COVID-19) <b> is an infectious disease caused by a newly discovered coronavirus. <b>
 Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment.<b> The virus that causes COVID-19 is mainly transmitted through droplets generated when an infected person coughs, sneezes, or exhales.<b> These droplets are too heavy to hang in the air, and quickly fall on floors or surfaces.<b>
-You can be infected by breathing in the virus if you are within close proximity of someone who has COVID-19, or by touching a contaminated surface and then your eyes, nose or mouth.<b> 
-</font> </center>
+You can be infected by breathing in the virus if you are within close proximity of someone who has COVID-19, or by touching a contaminated surface and then your eyes, nose or mouth.<b> <p>
+Researchers are still learning about COVID-19. What IS known is that people infected with COVID-19 can spread the virus to others before experiencing symptoms themselves (while people are still “asymptomatic”). Once you do have symptoms, the CDC says you are no longer contagious 10 days after your symptoms began.
+  </font> </center>
 <p>
 <p>
 <p align="center"> <img src = "https://www.munsonhealthcare.org/media/Image/Kid%20Resources%20About%20Coronavirus%20356x240(1).jpg" , width = 210 , length = 210></img><br>
