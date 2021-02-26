3d2a1f1912011c1d472dbc2cd919f76fea306f0a
Harlene4 <77616751+Harlene4@users.noreply.github.com>
2021-02-26 10:04:36 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <center>
 <body bgcolor= "lightpink">
   <iframe width="560" height="315" src="https://www.youtube.com/embed/i0ZabxXmH4Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<p> <font color= "black"> <font size="6">   Coronavirus disease (COVID-19) <b> is an infectious disease caused by a newly discovered coronavirus. <b>
+<p> <font color= "black"> <font size="4">   Coronavirus disease (COVID-19) <b> is an infectious disease caused by a newly discovered coronavirus. <b>
 Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment.<b> The virus that causes COVID-19 is mainly transmitted through droplets generated when an infected person coughs, sneezes, or exhales.<b> These droplets are too heavy to hang in the air, and quickly fall on floors or surfaces.<b>
 You can be infected by breathing in the virus if you are within close proximity of someone who has COVID-19, or by touching a contaminated surface and then your eyes, nose or mouth.<b> <p>
 Researchers are still learning about COVID-19. What IS known is that people infected with COVID-19 can spread the virus to others before experiencing symptoms themselves (while people are still “asymptomatic”). Once you do have symptoms, the CDC says you are no longer contagious 10 days after your symptoms began.
